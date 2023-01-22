b1e36b913e4ab918f71980cbe8492c2c03893110
Joseph <josephshandyharvian@gmail.com>
2023-01-22 15:27:28 +0700

    Update Changes

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -2,7 +2,7 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+        <ion-title>Camera Sample</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
@@ -12,12 +12,116 @@
         </ion-toolbar>
       </ion-header>
 
-      <ExploreContainer name="Tab 1 page" />
+
+      <div id="camera" class="camera" v-if="!imageCapture">
+        <div class="burn-area">
+          <img :src="require(`@/assets/images/${bodyType}.png`)" />
+        </div>
+      </div>
+
+      <div class="result" v-else>
+        <img :src="imageCapture" />
+        <ion-button @click="imageCapture = null; startCamera()">Back</ion-button>
+      </div>
+
     </ion-content>
+
+    <ion-footer>
+      <ion-toolbar>
+        <ion-segment @ionChange="segmentChanged($event)" value="Man">
+          <ion-segment-button value="Man">
+            <ion-label>Lanang</ion-label>
+          </ion-segment-button>
+          <ion-segment-button value="Woman">
+            <ion-label>Banci</ion-label>
+          </ion-segment-button>
+        </ion-segment>
+      </ion-toolbar>
+
+
+      <ion-toolbar class="ion-text-center">
+        <ion-button v-if="isOn" @click="stopCamera()">
+          <ion-icon :icon="stop"></ion-icon>
+        </ion-button>
+
+        <ion-button v-else @click="startCamera()">
+          <ion-icon :icon="camera"></ion-icon>
+        </ion-button>
+
+        <ion-button @click="captureCamera()">
+          <ion-icon :icon="aperture"></ion-icon>
+        </ion-button>
+
+        <ion-button @click="flipCamera()">
+          <ion-icon :icon="repeat"></ion-icon>
+        </ion-button>
+      </ion-toolbar>
+    </ion-footer>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonFooter, IonSegment, IonSegmentButton, IonLabel } from '@ionic/vue';
+import { CameraPreview, CameraPreviewOptions } from '@capacitor-community/camera-preview';
+import { aperture, camera, repeat, stop } from 'ionicons/icons';
+import { onMounted, ref } from 'vue';
+
+const cameraPreviewOptions: CameraPreviewOptions = {
+  position: 'rear',
+  parent: 'camera',
+  className: 'camera'
+};
+
+const isOn: any = ref(true);
+const bodyType: any = ref('Man');
+const imageCapture: any = ref();
+
+const segmentChanged = (event: any) => {
+  console.log(event);
+  bodyType.value = event.target.value
+}
+
+const captureCamera = async () => {
+  const result = await CameraPreview.capture({ quality: 90 })
+  imageCapture.value = `data:image/jpeg;base64,${result.value}`
+  stopCamera();
+}
+
+const startCamera = () => {
+  isOn.value = true
+  CameraPreview.start(cameraPreviewOptions);
+}
+
+const stopCamera = () => {
+  isOn.value = false
+  CameraPreview.stop()
+}
+
+const flipCamera = () => {
+  CameraPreview.flip()
+}
+
+onMounted(() => {
+  startCamera()
+})
 </script>
+
+<style scoped lang="scss">
+.camera {
+  position: relative;
+}
+
+.burn-area {
+  position: absolute;
+  top: 0;
+  margin: 0 auto;
+  width: 100%;
+  height: 100%;
+
+  img {
+    width: 100%;
+    height: 100%;
+    opacity: 0.5;
+  }
+}
+</style>
--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -8,16 +8,95 @@
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 2</ion-title>
+          <ion-title size="large">Tab Skecth</ion-title>
         </ion-toolbar>
       </ion-header>
 
-      <ExploreContainer name="Tab 2 page" />
+      <Editor :canvasWidth="canvasWidth" :canvasHeight="canvasHeight" ref="editor" editorId="canvasId" />
+      <canvas ref="burnAreaCanvas"></canvas>
+
     </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
+<script  lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { defineComponent } from 'vue';
+// import Editor from 'vue-image-markup';
+
+export default defineComponent({
+  components: { IonPage, IonHeader, IonToolbar, IonTitle, IonContent },
+  setup() {
+
+    const canvasWidth = 500;
+    const canvasHeight = 500;
+    return {
+      canvasHeight, canvasWidth
+    }
+  },
+  data() {
+    return {
+      isDrawing: false,
+      totalBurnArea: 0,
+    }
+  },
+  computed: {
+    canvas() {
+      return this.$refs.burnAreaCanvas as any
+    },
+    context() {
+      return this.canvas.getContext('2d')
+    },
+  },
+  mounted() {
+    this.setupCanvas()
+  },
+  methods: {
+    setupCanvas() {
+      this.canvas = this.$refs.burnAreaCanvas
+      this.context = this.canvas.getContext('2d')
+      // set the canvas background to transparent
+      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
+      // create a new image object
+      const image = new Image();
+      image.src = '/assets/images/Man.png';
+      image.onload = () => {
+        this.context.drawImage(image, 0, 0);
+        // add event listeners
+        this.canvas.addEventListener('mousedown', this.startDrawing)
+        this.canvas.addEventListener('mousemove', this.draw)
+        this.canvas.addEventListener('mouseup', this.stopDrawing)
+      }
+    },
+    startDrawing(event: { clientX: any; clientY: any; }) {
+      this.isDrawing = true
+      this.context.beginPath()
+      this.context.moveTo(event.clientX, event.clientY)
+    },
+    draw(event: { clientX: any; clientY: any; }) {
+      if (!this.isDrawing) return
+      // check if the pixel under the cursor is transparent
+      const x = event.clientX;
+      const y = event.clientY;
+      const pixel = this.context.getImageData(x, y, 1, 1);
+      if (pixel.data[3] === 0) {
+        this.context.lineTo(x, y)
+        this.context.stroke()
+      }
+    },
+    stopDrawing() {
+      this.isDrawing = false
+      this.context.closePath()
+
+      // calculate burn area
+      const imageData = this.context.getImageData(0, 0, this.canvas.width, this.canvas.height)
+      for (let i = 0; i < imageData.data.length; i += 4) {
+        if (imageData.data[i] === 0) {
+          this.totalBurnArea++
+        }
+      }
+    }
+  }
+})
+
 </script>
